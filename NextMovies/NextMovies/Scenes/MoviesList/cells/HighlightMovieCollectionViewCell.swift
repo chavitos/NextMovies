43f39d438aa88bca3fd08818c46d4675e933b12b
Tiago Chaves <tiago.chaves@icarros.com.br>
2018-11-09 17:14:54 -0200

    Criação da estrutura do projeto com groups, scenes, views do fluxo na storyboard e controller da lista de filmes; Pod inicializados para criação do workspace.

---
+++ NextMovies/NextMovies/Scenes/MoviesList/cells/HighlightMovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HighlightMovieCollectionViewCell.swift
+//  NextMovies
+//
+//  Created by Tiago Chaves on 09/11/18.
+//  Copyright © 2018 Next. All rights reserved.
+//
+
+import UIKit
+
+class HighlightMovieCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ NextMovies/NextMovies/Scenes/MoviesList/cells/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  NextMovies
+//
+//  Created by Tiago Chaves on 09/11/18.
+//  Copyright © 2018 Next. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
